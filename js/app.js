4f68796557842907c4119046b02a08a8af2f55a7
Mahmut Ozan Yazar <101927776+mozanyazar@users.noreply.github.com>
2022-03-22 22:25:25 +0300

    Netflix landing page Clone

---
+++ js/app.js
@@ -0,0 +1,65 @@
+//question 1
+const answer1 = document.getElementById('answer1');
+const btn = document.getElementById('q1');
+//question 2
+const answer2 = document.getElementById('answer2');
+const btn2 = document.getElementById('q2');
+// question 3
+const answer3 = document.getElementById('answer3');
+const btn3 = document.getElementById('q3');
+// //question 4
+const answer4 = document.getElementById('answer4');
+const btn4 = document.getElementById('q4');
+// //question 5
+const answer5 = document.getElementById('answer5');
+const btn5 = document.getElementById('q5');
+//question6
+const answer6 = document.getElementById('answer6');
+const btn6 = document.getElementById('q6');
+btn.onclick = function() {
+    if (answer1.style.display !== 'none') {
+        answer1.style.display = 'none';
+    } else {
+        answer1.style.display = 'block';
+    }
+}
+
+btn2.onclick = function() {
+    if (answer2.style.display !== 'none') {
+        answer2.style.display = 'none';
+    } else {
+        answer2.style.display = 'block';
+    }
+}
+
+
+btn3.onclick = function() {
+    if (answer3.style.display !== 'none') {
+        answer3.style.display = 'none';
+    } else {
+        answer3.style.display = 'block';
+    }
+}
+
+btn4.onclick = function() {
+    if (answer4.style.display !== 'none') {
+        answer4.style.display = 'none';
+    } else {
+        answer4.style.display = 'block';
+    }
+}
+
+btn5.onclick = function() {
+    if (answer5.style.display !== 'none') {
+        answer5.style.display = 'none';
+    } else {
+        answer5.style.display = 'block';
+    }
+}
+btn6.onclick = function() {
+    if (answer6.style.display !== 'none') {
+        answer6.style.display = 'none';
+    } else {
+        answer6.style.display = 'block';
+    }
+}
